aa5f238c33008e806658a4bd8e8531ec6dec71bb
Viktor Rudenko <vitek_10@ukr.net>
2025-06-02 09:13:47 +0300

    fix

---
+++ src/main/kotlin/old_course/variables_and_strings/ex.24.kt
@@ -0,0 +1,8 @@
+package variables_and_strings
+
+fun main() {
+    val ears = "(\\(\\"
+    val face = "(-.-)"
+    val body = "o_(\")(\"))"
+    println("$ears\n$face\n$body")
+}
--- src/variables_and_strings/ex.24.kt
+++
@@ -1,8 +0,0 @@
-package variables_and_strings
-
-fun main() {
-    val ears = "(\\(\\"
-    val face = "(-.-)"
-    val body = "o_(\")(\"))"
-    println("$ears\n$face\n$body")
-}
